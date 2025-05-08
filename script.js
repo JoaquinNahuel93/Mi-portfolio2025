12faf2d1043eb25f960c23a73daf28628b8ac191
Gonzalez Joaquin Nahuel <joaquinnahuel1993@gmail.com>
2025-05-07 23:09:25 -0300

    Se agregan detalles y estilos a la pagina

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleContacto() {
+  var lista = document.getElementById("listaContacto");
+  if (lista.style.display === "none") {
+    lista.style.display = "block";
+  } else {
+    lista.style.display = "none";
+  }
+}
